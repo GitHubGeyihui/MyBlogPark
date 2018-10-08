08852c283f32ee3035a965a4f465dc7168832d5a
雨痕 谷 <1987643976@qq.com>
2018-10-08 22:43:48 +0800

    热门博文实现

--- MyBlogPark/Controllers/BlogController.cs
+++ MyBlogPark/Controllers/BlogController.cs
@@ -10,7 +10,7 @@ namespace MyBlogPark.Controllers
     public class BlogController : BaseController
     {
         // GET: Blog    
-        public ActionResult Index(string blog)
+        public ActionResult Index(string blog, int p = 1)
         {
             // ViewBag.BlogName = blog;//传递数据的动态类型
             //在博客前台首页这里读取博客的实体，根据博客的标识符在前台呈现
@@ -19,31 +19,46 @@ namespace MyBlogPark.Controllers
             {
                 return Content("博客不存在");
             }
-            ViewBag.AritcleList = dbContext.Article.Where(m => m.BlogID == model.ID).OrderByDescending(m => m.ID).ToList();
+            int pageSize = 10;
+
+            ViewBag.ArticleList = dbContext.Article.Where(m => m.BlogID == model.ID).OrderByDescending(m => m.ID).Skip((p - 1) * pageSize).Take(pageSize).ToList();
+
+            ViewBag.TotalCount = dbContext.Article.Where(m => m.BlogID == model.ID).Count();
+            ViewBag.PageSize = pageSize;
 
             ViewBag.CatalogList = dbContext.Catalog.Where(m => m.BlogID == model.ID).OrderByDescending(m => m.ID).ToList();
+            GetViewBag(model);
             return View(model);
         }
-        public ActionResult Article(string blog,int id)
+        public ActionResult Article(string blog, int id)
         {
-        // 先判断用户存不存在
+            // 先判断用户存不存在
             var blogModel = dbContext.Blog.Where(m => m.Identity.ToLower() == blog.ToLower()).FirstOrDefault();
             if (blogModel == null)
             {
-                return Content("博文不存在"); 
+                return Content("博客不存在");
             }
-            
-        //在判断文章存不存在
+            GetViewBag(blogModel);
+            //在判断文章存不存在
             var model = dbContext.Article.Where(m => m.ID == id).FirstOrDefault();
             if (model == null)
             {
                 return Content("博文不存在");
             }
-            ViewBag.Article = model; 
+            ViewBag.Article = model;
 
             ViewBag.CatalogList = dbContext.Catalog.Where(m => m.BlogID == model.ID).OrderByDescending(m => m.ID).ToList();
 
             return View(blogModel);
         }
-    } 
+        //右侧栏热门博文
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+        private void GetViewBag(Blog blog)
+        {
+            ViewBag.HotArticleList = dbContext.Article.Where(m => m.BlogID == blog.ID).OrderByDescending(m => m.Views).Take(6).ToList();
+        }
+    }
 }
